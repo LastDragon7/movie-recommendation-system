492d8c065a184414463c69cf2a63ac4a36a011ae
LastDragon7 <133903959+LastDragon7@users.noreply.github.com>
2024-06-17 13:19:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
 import streamlit as st
 import pickle
+import gdown
+import os
 
+# Function to download files from Google Drive
+def download_files():
+    url_movies = 'https://drive.google.com/file/d/1PgyfKRXUXBUQ3sGXJMMVnWoeAtAkmQA5/view?usp=drive_link'
+    url_similarity = 'https://drive.google.com/file/d/1uIPbBw58MRGOWPMN73lr97kuYYj9L63o/view?usp=drive_link'
+    output_movies = 'movies.pkl'
+    output_similarity = 'similarity.pkl'
+    
+    if not os.path.exists(output_movies):
+        gdown.download(url_movies, output_movies, quiet=False)
+        
+    if not os.path.exists(output_similarity):
+        gdown.download(url_similarity, output_similarity, quiet=False)
+
+# Download the files
+download_files()
+
+# Function to recommend movies
 def recommend(movie):
-    i = movies[movies['title']==movie].index[0]
+    i = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[i])), reverse=True, key=lambda x: x[1])
-    recommend_movie_name = []
-    for i in distances[1:6]:
-        id = movies.iloc[i[0]].id
-        recommend_movie_name.append(movies.iloc[i[0]].title)
-    return recommend_movie_name
+    recommend_movie_names = [movies.iloc[i[0]].title for i in distances[1:6]]
+    return recommend_movie_names
 
+# Title of the app
 st.title('Movie Recommendation System')
-movies = pickle.load(open('movie-recommendation-system\movies-recommendation-system\movies.pkl','rb'))
-similarity = pickle.load(open('movie-recommendation-system\movies-recommendation-system\similarity.pkl','rb'))
 
+# Load the downloaded files
+movies = pickle.load(open('movies.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Movie selection dropdown
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
     movie_list
 )
 
-if st.button('Recommned'):
+# Recommendation button
+if st.button('Recommend'):
     names = recommend(selected_movie)
-    for i in names:
-        st.write(i)
-
-
+    for name in names:
+        st.write(name)
